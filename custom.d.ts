b49d14e4992bb84c845e543877fcb464ad7064b6
Lukas Cornille <lukascornille@hotmail.com>
2020-05-28 21:25:24 +0200

    Add custom types to one file

--- custom.d.ts
+++ custom.d.ts
@@ -1,3 +1,8 @@
+declare module '*.svg' {
+  const content: any;
+  export default content;
+}
+
 declare module 'apollo-link-prismic' {
   export function PrismicLink(args: { uri: string; accessToken?: string }): any;
 }
