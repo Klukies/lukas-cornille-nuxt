b42871e6902de6db6093fb0388f49b722941decb
Lukas Cornille <lukascornille@hotmail.com>
2020-05-31 21:36:42 +0200

    Add PostPreview component in the old vue 2 way
    A blogpost will explain the difference between vue 3 and vue2.
    These files are the way of working in vue 2, while the vue 3 way is
    implementend in my app

---
+++ components/Shared/PostPreviewOA.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="post-preview">
+    <div class="post-preview-header">
+      <PrismicRichText :field="post.node.title" />
+      <div class="post-preview-info">
+        <span>{{ formattedDate }}</span>
+        <span>{{ post.node.category }}</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+/*
+ * This Component is not used in my application
+ * It is used to provide an example of vue 2 vs vue 3
+ */
+import Vue from 'vue';
+import { Post } from '~/graphql/types';
+import { formatDate } from '~/helpers/post';
+
+export default Vue.extend({
+  props: {
+    post: {
+      type: Object as () => Post,
+      required: true,
+    },
+  },
+  computed: {
+    formattedDate() {
+      return formatDate(
+        this.post.node._meta.firstPublicationDate,
+        this.post.node.wppublicationdate,
+      );
+    },
+  },
+});
+</script>
+
+<style scoped>
+>>> h3 {
+  @apply text-2xl mb-1;
+}
+</style>
