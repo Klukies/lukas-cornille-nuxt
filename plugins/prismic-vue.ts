72ffa6aadf2c81578462bb91bf71dac493064479
Lukas Cornille <lukascornille@hotmail.com>
2020-06-03 20:10:59 +0200

    Update prismic-vue to moved package

--- plugins/prismic-vue.ts
+++ plugins/prismic-vue.ts
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 // @ts-ignore
-import PrismicVue from 'prismic-vue';
+import PrismicVue from '@prismicio/vue';
 import htmlSerializer from './html-serializer';
 
 Vue.use(PrismicVue, {
